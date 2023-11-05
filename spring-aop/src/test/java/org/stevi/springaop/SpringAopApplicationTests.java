0d34577536f3c365157a52ff43942cec7abae681
stevi-u <trozzato@gmail.com>
2023-11-05 13:20:12 +0200

    added spring-aop init folder

---
+++ spring-aop/src/test/java/org/stevi/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stevi.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
